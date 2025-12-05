dra27/ocaml

Add %standard_library_default
%standard_library_default allows Config.standard_library_default to be
converted to a compile-time derived value, as with existing compile-time
constants such as %backend_type, etc. This paves the way for allowing
Config.standard_library_default to be changed at link-time, rather than
fixed when the Config module itself is compiled.

middle_end/closure/closure.ml
@@ -1073,22 +1073,29 @@ let rec close ({ backend; fenv; cenv ; mutable_vars } as env) lam =
         (Uletrec(udefs, ubody), approx)
       end
   (* Compile-time constants *)
-  | Lprim(Pctconst c, [arg], _loc) ->
-      let cst, approx =
-        match c with
-        | Big_endian -> make_const_bool B.big_endian
-        | Word_size -> make_const_int (8*B.size_int)
-        | Int_size -> make_const_int (8*B.size_int - 1)
-        | Max_wosize -> make_const_int ((1 lsl ((8*B.size_int) - 10)) - 1 )
-        | Ostype_unix -> make_const_bool (Sys.os_type = "Unix")
-        | Ostype_win32 -> make_const_bool (Sys.os_type = "Win32")
-        | Ostype_cygwin -> make_const_bool (Sys.os_type = "Cygwin")
-        | Backend_type ->
-            make_const_int 0 (* tag 0 is the same as Native here *)
+  | Lprim(Pctconst c, [arg], loc) ->
+      let cst f v =
+        let cst, approx = f v in
+        let arg, _approx = close env arg in
+        let id = Ident.create_local "dummy" in
+        Ulet(Immutable, Pgenval, VP.create id, arg, cst), approx
       in
-      let arg, _approx = close env arg in
-      let id = Ident.create_local "dummy" in
-      Ulet(Immutable, Pgenval, VP.create id, arg, cst), approx
+      begin match c with
+      | Big_endian -> cst make_const_bool B.big_endian
+      | Word_size -> cst make_const_int (8*B.size_int)
+      | Int_size -> cst make_const_int (8*B.size_int - 1)
+      | Max_wosize -> cst make_const_int ((1 lsl ((8*B.size_int) - 10)) - 1)
+      | Ostype_unix -> cst make_const_bool (Sys.os_type = "Unix")
+      | Ostype_win32 -> cst make_const_bool (Sys.os_type = "Win32")
+      | Ostype_cygwin -> cst make_const_bool (Sys.os_type = "Cygwin")
+      | Backend_type ->
+          cst make_const_int 0 (* tag 0 is the same as Native here *)
+      | Standard_library_default ->
+          Compilenv.need_stdlib_location ();
+          let dbg = Debuginfo.from_location loc in
+          let id = Ident.name Compilenv.stdlib_symbol_name in
+          Uprim(P.Pread_symbol id, [], dbg), Value_const (Uconst_ref (id, None))
+      end
   | Lprim(Pignore, [arg], _loc) ->
       let expr, approx = make_const_int 0 in
       Usequence(fst (close env arg), expr), approx
@@ -1455,7 +1462,9 @@ let collect_exported_structured_constants a =
     | Uconst_ref (s, (Some c)) ->
         Compilenv.add_exported_constant s;
         structured_constant c
-    | Uconst_ref (_s, None) -> assert false (* Cannot be generated *)
+    | Uconst_ref (s, None) ->
+        (* Only generated in one context *)
+        assert (s = Ident.name Compilenv.stdlib_symbol_name)
     | Uconst_int _ -> ()
   and structured_constant = function
     | Uconst_block (_, ul) -> List.iter const ul

NickBarnes/oxcaml

middle_end/closure/closure.ml
@@ -1056,22 +1056,29 @@ let rec close ({ backend; fenv; cenv ; mutable_vars } as env) lam =
               None ubody),
        approx)
   (* Compile-time constants *)
-  | Lprim(Pctconst c, [arg], _loc) ->
-      let cst, approx =
-        match c with
-        | Big_endian -> make_const_bool B.big_endian
-        | Word_size -> make_const_int (8*B.size_int)
-        | Int_size -> make_const_int (8*B.size_int - 1)
-        | Max_wosize -> make_const_int ((1 lsl ((8*B.size_int) - 10)) - 1 )
-        | Ostype_unix -> make_const_bool (Config.target_os_type = "Unix")
-        | Ostype_win32 -> make_const_bool (Config.target_os_type = "Win32")
-        | Ostype_cygwin -> make_const_bool (Config.target_os_type = "Cygwin")
-        | Backend_type ->
-            make_const_int 0 (* tag 0 is the same as Native here *)
+  | Lprim(Pctconst c, [arg], loc) ->
+      let cst f v =
+        let cst, approx = f v in
+        let arg, _approx = close env arg in
+        let id = Ident.create_local "dummy" in
+        Ulet(Immutable, Pgenval, VP.create id, arg, cst), approx
       in
-      let arg, _approx = close env arg in
-      let id = Ident.create_local "dummy" in
-      Ulet(Immutable, Pgenval, VP.create id, arg, cst), approx
+      begin match c with
+      | Big_endian -> cst make_const_bool B.big_endian
+      | Word_size -> cst make_const_int (8*B.size_int)
+      | Int_size -> cst make_const_int (8*B.size_int - 1)
+      | Max_wosize -> cst make_const_int ((1 lsl ((8*B.size_int) - 10)) - 1)
+      | Ostype_unix -> cst make_const_bool (Config.target_os_type = "Unix")
+      | Ostype_win32 -> cst make_const_bool (Config.target_os_type = "Win32")
+      | Ostype_cygwin -> cst make_const_bool (Config.target_os_type = "Cygwin")
+      | Backend_type ->
+          cst make_const_int 0 (* tag 0 is the same as Native here *)
+      | Standard_library_default ->
+          Compilenv.need_stdlib_location ();
+          let dbg = Debuginfo.from_location loc in
+          let id = Ident.name Compilenv.stdlib_symbol_name in
+          Uprim(P.Pread_symbol id, [], dbg), Value_const (Uconst_ref (id, None))
+      end
   | Lprim(Pignore, [arg], _loc) ->
       let expr, approx = make_const_int 0 in
       Usequence(fst (close env arg), expr), approx
@@ -1463,7 +1470,9 @@ let collect_exported_structured_constants a =
     | Uconst_ref (s, (Some c)) ->
         Compilenv.add_exported_constant s;
         structured_constant c
-    | Uconst_ref (_s, None) -> assert false (* Cannot be generated *)
+    | Uconst_ref (s, None) ->
+        (* Only generated in one context *)
+        assert (s = Ident.name Compilenv.stdlib_symbol_name)
     | Uconst_int _ -> ()
   and structured_constant = function
     | Uconst_block (_, ul) -> List.iter const ul