dra27/ocaml

Add -runtime-search to ocamlc
-runtime-search {disable|enable|always} adds new features to the
launcher used for bytecode executables which do not embed their own
runtime. By default, the header continues to behave as before - the
launcher will attempt to start the runtime using the absolute path which
the compiler was configured with.

The new search mode will then search for the runtime first in the
directory containing the running executable and then in PATH.

bytecomp/byterntm.mll
@@ -12,31 +12,84 @@
 (*                                                                        *)
 (**************************************************************************)
 
+{
+type search_method =
+| Absolute of string
+| Absolute_then_search of string
+| Search
+
+type state = Exec | R | C of string
+}
+
 rule analyze = parse
-(* RNTM section or shebang directly to the runtime *)
-  | "#!" ([^ ' ' '\n']+ as runtime) '\n'
-  | ([^ '\000']+ as runtime) '\000' eof
-      { Some runtime }
+(* RNTM section for -runtime-search absolute or shebang directly to the
+   runtime *)
+  | "#!" ([^ ' ' '\n']* as dir) ('/' as sep) ([^ '/' ' ' '\n']+ as runtime) '\n'
+  | ([^ '\000']* as dir) (['/' '\\' '\000'] as sep) (* Directory portion *)
+    ([^ '\\' '/' '\000']+ as runtime) eof           (* Runtime portion *)
+      { if sep = '\000' then
+          if dir = "" then
+            Some (runtime, Search)
+          else
+            Some (runtime, Absolute_then_search (Filename.concat dir ""))
+        else
+          Some (runtime, Absolute (dir ^ String.make 1 sep)) }
+
+(* Legacy RNTM (remove after bootstrap) *)
+  | (([^ '\000']* ['/' '\\']) as dir)
+    ([^ '\\' '/' '\000']+ as runtime) '\000' eof
+      { if dir = "" then
+          Some (runtime, Search)
+        else
+          Some (runtime, Absolute dir) }
 
 (* Shell script launcher (if it matches, this always matches more than the above
    regexp) *)
-  | "#!" [^ ' ' '\n']+ "/sh\nexec '"
-      { analyze_sh_launcher (Buffer.create 1024) lexbuf }
+  | "#!" [^ ' ' '\n']+ "/sh\n" (("exec '" | "r='") as next)
+      { let state = if next.[0] = 'r' then R else Exec in
+        analyze_sh_launcher state (Buffer.create 1024) lexbuf }
 
   | _ | eof
       { None }
 
-and analyze_sh_launcher b = parse
-(* An embedded single quote in the filename passed to exec *)
+and analyze_sh_launcher state b = parse
+(* An embedded single quote *)
   | "'\\''"
-      { analyze_sh_launcher (Buffer.add_char b '\''; b) lexbuf }
+      { analyze_sh_launcher state (Buffer.add_char b '\''; b) lexbuf }
 
-  | [^ '\'']+ as s
-      { analyze_sh_launcher (Buffer.add_string b s; b) lexbuf }
+  | [^ '\'' ]+ as s
+      { analyze_sh_launcher state (Buffer.add_string b s; b) lexbuf }
 
-(* End of the filename parsed; return the entire string *)
+(* exec line for -runtime-search disable *)
   | "' \"$0\" \"$@\"\n"
-      { Some (Buffer.contents b) }
+      { if state = Exec then
+          let name = Buffer.contents b in
+          let runtime = Filename.basename name in
+          let dir =
+            String.sub name 0 (String.length name - String.length runtime)
+          in
+          Some (runtime, Absolute dir)
+        else
+          None }
+
+(* r= line for -runtime-search {always,enable} *)
+  | "'\n" ("c='" as c)?
+      { if state = R then
+          let runtime = Buffer.contents b in
+          if c = None then
+            Some (runtime, Search)
+          else
+            analyze_sh_launcher (C runtime) (Buffer.clear b; b) lexbuf
+        else
+          None }
+
+(* c= line for -runtime-search enable *)
+  | "'\"$r\"\n"
+      { match state with
+        | C runtime ->
+            Some (runtime, Absolute_then_search (Buffer.contents b))
+        | _ ->
+            None }
 
   | _ | eof
       { None }

NickBarnes/oxcaml

bytecomp/byterntm.mll
@@ -12,31 +12,86 @@
 (*                                                                        *)
 (**************************************************************************)
 
+{
+type search_method =
+| Disable of string
+| Fallback of string
+| Enable
+
+(* First word of the current line being analysed - [exec ...], [r=...], or
+   [c=...] *)
+type state = Exec | R | C of string
+}
+
 rule analyze = parse
-(* RNTM section or shebang directly to the runtime *)
-  | "#!" ([^ ' ' '\n']+ as runtime) '\n'
-  | ([^ '\000']+ as runtime) '\000' eof
-      { Some runtime }
+(* RNTM section for -runtime-search absolute or shebang directly to the
+   runtime *)
+  | "#!" ([^ ' ' '\n']* as dir) ('/' as sep) ([^ '/' ' ' '\n']+ as runtime) '\n'
+  | ([^ '\000']* as dir) (['/' '\\' '\000'] as sep) (* Directory portion *)
+    ([^ '\\' '/' '\000']+ as runtime) eof           (* Runtime portion *)
+      { if sep = '\000' then
+          if dir = "" then
+            Some (runtime, Enable)
+          else
+            Some (runtime, Fallback (Filename.concat dir ""))
+        else
+          Some (runtime, Disable (dir ^ String.make 1 sep)) }
+
+(* Legacy RNTM (remove after bootstrap) *)
+  | (([^ '\000']* ['/' '\\']) as dir)
+    ([^ '\\' '/' '\000']+ as runtime) '\000' eof
+      { if dir = "" then
+          Some (runtime, Enable)
+        else
+          Some (runtime, Disable dir) }
 
 (* Shell script launcher (if it matches, this always matches more than the above
    regexp) *)
-  | "#!" [^ ' ' '\n']+ "/sh\nexec '"
-      { analyze_sh_launcher (Buffer.create 1024) lexbuf }
+  | "#!" [^ ' ' '\n']+ "/sh\n" (("exec '" | "r='") as next)
+      { let state = if next.[0] = 'r' then R else Exec in
+        analyze_sh_launcher state (Buffer.create 1024) lexbuf }
 
   | _ | eof
       { None }
 
-and analyze_sh_launcher b = parse
-(* An embedded single quote in the filename passed to exec *)
+and analyze_sh_launcher state b = parse
+(* An embedded single quote *)
   | "'\\''"
-      { analyze_sh_launcher (Buffer.add_char b '\''; b) lexbuf }
+      { analyze_sh_launcher state (Buffer.add_char b '\''; b) lexbuf }
 
-  | [^ '\'']+ as s
-      { analyze_sh_launcher (Buffer.add_string b s; b) lexbuf }
+  | [^ '\'' ]+ as s
+      { analyze_sh_launcher state (Buffer.add_string b s; b) lexbuf }
 
-(* End of the filename parsed; return the entire string *)
+(* exec line for -runtime-search disable *)
   | "' \"$0\" \"$@\"\n"
-      { Some (Buffer.contents b) }
+      { if state = Exec then
+          let name = Buffer.contents b in
+          let runtime = Filename.basename name in
+          let dir =
+            String.sub name 0 (String.length name - String.length runtime)
+          in
+          Some (runtime, Disable dir)
+        else
+          None }
+
+(* r= line for -runtime-search {fallback,enable} *)
+  | "'\n" ("c='" as c)?
+      { if state = R then
+          let runtime = Buffer.contents b in
+          if c = None then
+            Some (runtime, Enable)
+          else
+            analyze_sh_launcher (C runtime) (Buffer.clear b; b) lexbuf
+        else
+          None }
+
+(* c= line for -runtime-search fallback *)
+  | "'\"$r\"\n"
+      { match state with
+        | C runtime ->
+            Some (runtime, Fallback (Buffer.contents b))
+        | _ ->
+            None }
 
   | _ | eof
       { None }