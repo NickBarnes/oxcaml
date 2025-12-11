dra27/ocaml

Update the installation test for runtime-searching

testsuite/tools/cmdline.ml
@@ -104,7 +104,7 @@ let parse argv =
   in
   let config =
     ref {has_ocamlnat = false; has_ocamlopt = false; has_relative_libdir = None;
-         has_runtime_search = None; launcher_searches_for_ocamlrun = false;
+         has_runtime_search = Absolute; launcher_searches_for_ocamlrun = false;
          target_launcher_searches_for_ocamlrun = false;
          bytecode_shebangs_by_default = false; shebangscripts = false;
          filename_mangling = false; libraries = []}
@@ -163,16 +163,15 @@ let parse argv =
     config := {!config with shebangscripts}
   in
   let parse_search = function
-  | "enable" -> true
-  | "always" -> false
+  | Some "enable" -> Config.Absolute_then_search
+  | Some "always" -> Config.Search
+  | None -> Config.Absolute
   | _ ->
       raise (Arg.Bad
         "--with-runtime-search: argument should be either enable or always")
   in
   let has_runtime_search arg =
-    let has_runtime_search = Option.map parse_search arg in
-    if has_runtime_search <> None then
-      error "--with-runtime-search is not implemented!";
+    let has_runtime_search = parse_search arg in
     config := {!config with has_runtime_search}
   in
   let args = Arg.align [

testsuite/tools/testBytecodeBinaries.ml
@@ -84,7 +84,7 @@ let run config env =
                     else
                       failed, "compiled with -custom"
                 | Tendered {runtime; id; header; search; _} ->
-                    let reported_runtime =
+                    let reported_runtime, search =
                       let id =
                         Option.map
                           (fun t -> "-" ^ Misc.RuntimeID.to_string t) id
@@ -92,24 +92,22 @@ let run config env =
                       in
                       match search with
                       | Absolute dir ->
-                          dir ^ runtime ^ id
+                          dir ^ runtime ^ id, Config.Absolute
                       | Absolute_then_search dir ->
-                          Printf.sprintf "[%s]%s%s" dir runtime id
+                          Printf.sprintf "[%s]%s%s" dir runtime id,
+                          Config.Absolute_then_search
                       | Search ->
-                          runtime ^ id
+                          runtime ^ id, Config.Search
                     in
                     let expected_id =
                       if config.filename_mangling then
-                        Some (Misc.RuntimeID.make_zinc ())
+                        match config.has_runtime_search with
+                        | Config.Absolute | Config.Absolute_then_search ->
+                            Some (Misc.RuntimeID.make_zinc ())
+                        | Config.Search ->
+                            Some (Misc.RuntimeID.make_zinc ())
                       else
                         None
-                    in
-                    let expected_search =
-                      if Sys.win32 then
-                        Byterntm.Search
-                      else
-                        Byterntm.Absolute
-                          (Filename.concat (Environment.bindir env) "")
                     in
                     let expected_launch_mode =
                       if config.shebangscripts then
@@ -124,11 +122,11 @@ let run config env =
                         Format.pp_print_string f (Misc.RuntimeID.to_string id)
                     in
                     let pp_search f = function
-                    | Byterntm.Absolute _ ->
+                    | Config.Absolute ->
                         Format.pp_print_string f "absolute"
-                    | Byterntm.Absolute_then_search _ ->
+                    | Config.Absolute_then_search ->
                         Format.pp_print_string f "fallback"
-                    | Byterntm.Search ->
+                    | Config.Search ->
                         Format.pp_print_string f "search"
                     in
                     let pp_launch f = function
@@ -145,7 +143,7 @@ let run config env =
                     in
                     let failed =
                       failed
-                      |> check expected_search search
+                      |> check config.has_runtime_search search
                                "search mechanism" pp_search
                       |> check expected_id id
                                "runtime ID" pp_runtime_id

testsuite/tools/testLinkModes.ml
@@ -501,7 +501,10 @@ let compile_test usr_bin_sh config env test test_program description =
             | _ ->
                 [] in
           let target_launcher_searches_for_ocamlrun =
-            (search_method <> Config.Absolute)
+            if search_method = Config.search_method then
+              None
+            else
+              Some (search_method <> Config.Absolute)
           in
           let param =
             match search_method with
@@ -509,8 +512,13 @@ let compile_test usr_bin_sh config env test test_program description =
             | Absolute_then_search -> "enable"
             | Search -> "always"
           in
-          let args = "-runtime-search" :: param :: args in
-          f ~target_launcher_searches_for_ocamlrun ~tendered:true args
+          let args =
+            if search_method = Config.search_method then
+              args
+            else
+              "-runtime-search" :: param :: args
+          in
+          f ?target_launcher_searches_for_ocamlrun ~tendered:true args
       | Default_ocamlopt ->
           f ~mode:Native []
       | Custom_runtime Static ->

testsuite/tools/testRelocation.ml
@@ -80,7 +80,7 @@ let bindir_rules config file =
         (* If the launcher doesn't search for ocamlrun, then either the #! stub
            will include the absolute path or the RNTM section will *)
         match classification with
-        | Tendered _ when not config.launcher_searches_for_ocamlrun -> true
+        | Tendered _ when config.has_runtime_search <> Config.Search -> true
         | _ -> false
       in
       if code_embeds_stdlib_location || linker_embeds_stdlib_location then
@@ -526,17 +526,6 @@ let run ~reproducible config env =
     |> scan Environment.libdir "$libdir" libdir_rules
   in
   flush stderr;
-  (* Abort the harness if there are files which didn't match a ruleset *)
-  let () =
-    if results_are_reproducible && not consistent then
-      Harness.fail_because
-        "Internal error: bindir_rules and libdir_rules disagree with \
-         reproducible_rules"
-    else if results_are_reproducible <> reproducible then
-      Harness.fail_because
-        "The build is %sexpected to be reproducible"
-        (if not reproducible then "not " else "")
-  in
   (* Summarise the results, using wildcards to bring them to a readable
      length *)
   let sections =
@@ -655,7 +644,15 @@ let run ~reproducible config env =
     let pp_results = Format.(pp_print_list ~pp_sep pp_print_string) in
     Format.printf "@[<hov 4>  %a@]@." pp_results results
   in
+  (* Abort the harness if there are files which didn't match a ruleset *)
   if failed then
-    Harness.fail_because "Installed files don't match expectation"
-  else
-  List.iter display sections
+    Harness.fail_because "Installed files don't match expectation";
+  List.iter display sections;
+  if results_are_reproducible && not consistent then
+    Harness.fail_because
+      "Internal error: bindir_rules and libdir_rules disagree with \
+       reproducible_rules"
+  else if results_are_reproducible <> reproducible then
+    Harness.fail_because
+      "The build is %sexpected to be reproducible"
+      (if not reproducible then "not " else "")

NickBarnes/oxcaml

testsuite/tools/cmdline.ml
@@ -104,7 +104,7 @@ let parse argv =
   in
   let config =
     ref {has_ocamlnat = false; has_ocamlopt = false; has_relative_libdir = None;
-         has_runtime_search = None; launcher_searches_for_ocamlrun = false;
+         has_runtime_search = Disable; launcher_searches_for_ocamlrun = false;
          target_launcher_searches_for_ocamlrun = false;
          bytecode_shebangs_by_default = false; filename_mangling = false;
          libraries = []}
@@ -161,16 +161,15 @@ let parse argv =
   let has_ocamlnat has_ocamlnat () = config := {!config with has_ocamlnat} in
   let has_ocamlopt has_ocamlopt () = config := {!config with has_ocamlopt} in
   let parse_search = function
-  | "fallback" -> true
-  | "enable" -> false
+  | Some "fallback" -> Config.Fallback
+  | Some "enable" -> Config.Enable
+  | None -> Config.Disable
   | _ ->
       raise (Arg.Bad
         "--with-runtime-search: argument should be either fallback or enable")
   in
   let has_runtime_search arg =
-    let has_runtime_search = Option.map parse_search arg in
-    if has_runtime_search <> None then
-      error "--with-runtime-search is not implemented!";
+    let has_runtime_search = parse_search arg in
     config := {!config with has_runtime_search}
   in
   let args = Arg.align [

testsuite/tools/testBytecodeBinaries.ml
@@ -84,7 +84,7 @@ let run config env =
                     else
                       failed, "compiled with -custom"
                 | Tendered {runtime; id; header; search; _} ->
-                    let reported_runtime =
+                    let reported_runtime, search =
                       let id =
                         Option.map
                           (fun t -> "-" ^ Misc.RuntimeID.to_string t) id
@@ -92,24 +92,18 @@ let run config env =
                       in
                       match search with
                       | Disable dir ->
-                          dir ^ runtime ^ id
+                          dir ^ runtime ^ id, Config.Disable
                       | Fallback dir ->
-                          Printf.sprintf "[%s]%s%s" dir runtime id
+                          Printf.sprintf "[%s]%s%s" dir runtime id,
+                          Config.Fallback
                       | Enable ->
-                          runtime ^ id
+                          runtime ^ id, Config.Enable
                     in
                     let expected_id =
                       if config.filename_mangling then
                         Some (Misc.RuntimeID.make_zinc ())
                       else
                         None
-                    in
-                    let expected_search =
-                      if Sys.win32 then
-                        Byterntm.Enable
-                      else
-                        Byterntm.Disable
-                          (Filename.concat (Environment.bindir env) "")
                     in
                     let expected_launch_mode =
                       if Config.shebangscripts then
@@ -124,11 +118,11 @@ let run config env =
                         Format.pp_print_string f (Misc.RuntimeID.to_string id)
                     in
                     let pp_search f = function
-                    | Byterntm.Disable _ ->
+                    | Config.Disable ->
                         Format.pp_print_string f "disable"
-                    | Byterntm.Fallback _ ->
+                    | Config.Fallback ->
                         Format.pp_print_string f "fallback"
-                    | Byterntm.Enable ->
+                    | Config.Enable ->
                         Format.pp_print_string f "enable"
                     in
                     let pp_launch f = function
@@ -145,7 +139,7 @@ let run config env =
                     in
                     let failed =
                       failed
-                      |> check expected_search search
+                      |> check config.has_runtime_search search
                                "search mechanism" pp_search
                       |> check expected_id id
                                "runtime ID" pp_runtime_id

testsuite/tools/testLinkModes.ml
@@ -501,7 +501,10 @@ let compile_test usr_bin_sh config env test test_program description =
             | _ ->
                 [] in
           let target_launcher_searches_for_ocamlrun =
-            (search_method <> Config.Disable)
+            if search_method = Config.search_method then
+              None
+            else
+              Some (search_method <> Config.Disable)
           in
           let param =
             match search_method with
@@ -509,8 +512,13 @@ let compile_test usr_bin_sh config env test test_program description =
             | Fallback -> "fallback"
             | Enable -> "enable"
           in
-          let args = "-runtime-search" :: param :: args in
-          f ~target_launcher_searches_for_ocamlrun ~tendered:true args
+          let args =
+            if search_method = Config.search_method then
+              args
+            else
+              "-runtime-search" :: param :: args
+          in
+          f ?target_launcher_searches_for_ocamlrun ~tendered:true args
       | Default_ocamlopt ->
           f ~mode:Native []
       | Custom_runtime Static ->

testsuite/tools/testRelocation.ml
@@ -77,7 +77,7 @@ let bindir_rules config file =
         (* If the launcher doesn't search for ocamlrun, then either the #! stub
            will include the absolute path or the RNTM section will *)
         match classification with
-        | Tendered _ when not config.launcher_searches_for_ocamlrun -> true
+        | Tendered _ when config.has_runtime_search <> Config.Enable -> true
         | _ -> false
       in
       if code_embeds_stdlib_location || linker_embeds_stdlib_location then
@@ -488,17 +488,6 @@ let run ~reproducible config env =
     |> scan Environment.libdir "$libdir" libdir_rules
   in
   flush stderr;
-  (* Abort the harness if there are files which didn't match a ruleset *)
-  let () =
-    if results_are_reproducible && not consistent then
-      Harness.fail_because
-        "Internal error: bindir_rules and libdir_rules disagree with \
-         reproducible_rules"
-    else if results_are_reproducible <> reproducible then
-      Harness.fail_because
-        "The build is %sexpected to be reproducible"
-        (if not reproducible then "not " else "")
-  in
   (* Summarise the results, using wildcards to bring them to a readable
      length *)
   let sections =
@@ -617,7 +606,15 @@ let run ~reproducible config env =
     let pp_results = Format.(pp_print_list ~pp_sep pp_print_string) in
     Format.printf "@[<hov 4>  %a@]@." pp_results results
   in
+  (* Abort the harness if there are files which didn't match a ruleset *)
   if failed then
-    Harness.fail_because "Installed files don't match expectation"
-  else
-  List.iter display sections
+    Harness.fail_because "Installed files don't match expectation";
+  List.iter display sections;
+  if results_are_reproducible && not consistent then
+    Harness.fail_because
+      "Internal error: bindir_rules and libdir_rules disagree with \
+       reproducible_rules"
+  else if results_are_reproducible <> reproducible then
+    Harness.fail_because
+      "The build is %sexpected to be reproducible"
+      (if not reproducible then "not " else "")