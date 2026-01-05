Octachron/ocaml

Much simpler fallback algorith for stable matching

utils/stable_matching.ml
@@ -316,64 +316,6 @@ module Stable_marriage_diff = struct
 
 end
 
-let (%<%) (x:int option) (y:int option) = match x, y with
-  | None , _ -> false
-  | _, None -> true
-  | Some x, Some y -> x < y
-
-let greedy_matching ~compatibility ~cutoff missings additions =
-  let rec list_extract predicate l =
-    match l with
-    | [] -> None
-    | hd :: tl when predicate hd -> Some (hd, tl)
-    | hd :: tl -> (
-        match list_extract predicate tl with
-        | None -> None
-        | Some (x, tl') -> Some (x, hd :: tl'))
-  in
-
-  let compute_distance expected_field added_field =
-    if compatibility (Item.kind added_field) (Item.kind expected_field) then
-      let distance =
-        let expected_name = Item.name expected_field in
-        Misc.edit_distance
-          expected_name
-          (Item.name added_field)
-          (cutoff expected_name)
-      in
-      distance
-    else
-      None
-  in
-
-  let remaining_added_fields = ref additions in
-  let name_changes = ref [] in
-  let actually_missing =
-    missings
-    |> List.filter_map
-      (fun missing_field ->
-        let missing_id = missing_field in
-        let missing_name = Item.name missing_id in
-        match
-          list_extract
-            (fun added_field ->
-              (compute_distance missing_field added_field)
-                %<% Some (cutoff missing_name))
-            !remaining_added_fields
-        with
-        | None -> Some missing_field
-        | Some (added_field, additions) ->
-            let name_change = Item.item added_field, Item.item missing_field in
-            name_changes := name_change :: !name_changes;
-            remaining_added_fields := additions;
-            None)
-  in
-  {
-    left = !remaining_added_fields;
-    pairs= !name_changes;
-    right = actually_missing
-  }
-
 let preferences ~cutoff left name =
   let cutoff = 1 + cutoff name in
   let a =
@@ -401,7 +343,13 @@ let preferences ~cutoff left name =
   in
   group_by 0 [] 0
 
-let fuzzy_match_names ~compatibility left0 right =
+let rec cut_at before pos l =
+  if pos <= 0 then List.rev before, l
+  else match l with
+    | [] -> List.rev before, []
+    | a :: q -> cut_at (a::before) (pos-1) q
+
+let fuzzy_match_names ~compatibility left right =
   (* The edit distance between an existing name and a suggested rename must be
      at most half the length of the name. *)
   let cutoff name =
@@ -412,10 +360,10 @@ let fuzzy_match_names ~compatibility left0 right =
     | 9 | 10 | 11 -> 3
     | len -> len/4
   in
-  if (*  *List.length left < 60 && List.length right < 60 *) true then
-    (* Stable marriages. *)
-    let left = Array.of_list left0 in
-    let right = Array.of_list right in
+  let left_pairing, left_rest =  cut_at [] 20 left in
+  let right_pairing, right_rest = cut_at [] 20 right in
+  let left = Array.of_list left_pairing in
+  let right = Array.of_list right_pairing in
     let compatibility i j =
       compatibility (Item.kind left.(i)) (Item.kind right.(j))
     in
@@ -426,8 +374,8 @@ let fuzzy_match_names ~compatibility left0 right =
         left
         right
     in
-    let pairs = List.map (fun (x,y) -> Item.(item x, item y)) matches.pairs in
-    { matches with pairs }
-  else
-    (* Greedy. *)
-    greedy_matching ~compatibility ~cutoff left0 right
+    {
+      pairs = List.map (fun (x,y) -> Item.(item x, item y)) matches.pairs;
+      left = matches.left @ left_rest;
+      right = matches.right @ right_rest
+    }

NickBarnes/oxcaml

utils/stable_matching.ml
@@ -318,64 +318,6 @@ module Stable_marriage_diff = struct
 
 end
 
-let (%<%) (x:int option) (y:int option) = match x, y with
-  | None , _ -> false
-  | _, None -> true
-  | Some x, Some y -> x < y
-
-let greedy_matching ~compatibility ~cutoff missings additions =
-  let rec list_extract predicate l =
-    match l with
-    | [] -> None
-    | hd :: tl when predicate hd -> Some (hd, tl)
-    | hd :: tl -> (
-        match list_extract predicate tl with
-        | None -> None
-        | Some (x, tl') -> Some (x, hd :: tl'))
-  in
-
-  let compute_distance expected_field added_field =
-    if compatibility (Item.kind added_field) (Item.kind expected_field) then
-      let distance =
-        let expected_name = Item.name expected_field in
-        Misc.edit_distance
-          expected_name
-          (Item.name added_field)
-          (cutoff expected_name)
-      in
-      distance
-    else
-      None
-  in
-
-  let remaining_added_fields = ref additions in
-  let name_changes = ref [] in
-  let actually_missing =
-    missings
-    |> List.filter_map
-      (fun missing_field ->
-        let missing_id = missing_field in
-        let missing_name = Item.name missing_id in
-        match
-          list_extract
-            (fun added_field ->
-              (compute_distance missing_field added_field)
-                %<% Some (cutoff missing_name))
-            !remaining_added_fields
-        with
-        | None -> Some missing_field
-        | Some (added_field, additions) ->
-            let name_change = Item.item added_field, Item.item missing_field in
-            name_changes := name_change :: !name_changes;
-            remaining_added_fields := additions;
-            None)
-  in
-  {
-    left = !remaining_added_fields;
-    pairs= !name_changes;
-    right = actually_missing
-  }
-
 let preferences ~cutoff left name =
   let cutoff = 1 + cutoff name in
   let a =
@@ -403,7 +345,13 @@ let preferences ~cutoff left name =
   in
   group_by 0 [] 0
 
-let fuzzy_match_names ~compatibility left0 right =
+let rec cut_at before pos l =
+  if pos <= 0 then List.rev before, l
+  else match l with
+    | [] -> List.rev before, []
+    | a :: q -> cut_at (a::before) (pos-1) q
+
+let fuzzy_match_names ~compatibility ~max_right_items left right =
   (* The edit distance between an existing name and a suggested rename must be
      at most half the length of the name. *)
   let cutoff name =
@@ -414,10 +362,9 @@ let fuzzy_match_names ~compatibility left0 right =
     | 9 | 10 | 11 -> 3
     | len -> len/4
   in
-  if (*  *List.length left < 60 && List.length right < 60 *) true then
-    (* Stable marriages. *)
-    let left = Array.of_list left0 in
-    let right = Array.of_list right in
+  let right_pairing, right_rest = cut_at [] max_right_items right in
+  let left = Array.of_list left in
+  let right = Array.of_list right_pairing in
     let compatible i j =
       compatibility (Item.kind left.(i)) (Item.kind right.(j))
     in
@@ -425,8 +372,8 @@ let fuzzy_match_names ~compatibility left0 right =
     let matches =
       Stable_marriage_diff.diff ~preferences ~compatible left right
     in
-    let pairs = List.map (fun (x,y) -> Item.(item x, item y)) matches.pairs in
-    { matches with pairs }
-  else
-    (* Greedy. *)
-    greedy_matching ~compatibility ~cutoff left0 right
+    {
+      pairs = List.map (fun (x,y) -> Item.(item x, item y)) matches.pairs;
+      left = matches.left;
+      right = matches.right @ right_rest
+    }