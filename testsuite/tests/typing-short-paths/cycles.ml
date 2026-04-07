smuenzel/ocaml

Update testsuite

testsuite/tests/typing-short-paths/cycles.ml
@@ -31,7 +31,6 @@ Line 2, characters 2-63:
 2 |   module rec X : sig type t = Y.t end = struct type t = Y.t end
       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 Error: The definition of "X.t" contains a cycle:
-         "Y.t" = "X.t",
          "X.t" = "Y.t",
          "Y.t" = "X.t"
 |}]
@@ -59,9 +58,8 @@ Lines 2-6, characters 2-5:
 5 |     type ('a, 'b) t = ('a, 'b) Y.t
 6 |   end
 Error: The definition of "X.t" contains a cycle:
-         "('a, 'b) Y.t" = "('a, 'b) X.t",
-         "('a, 'b) X.t" = "('a, 'b) Y.t",
-         "('a, 'b) Y.t" = "('a, 'b) X.t"
+         "('a, 'b) X.t" = "('c, 'd) Y.t",
+         "('c, 'd) Y.t" = "('c, 'd) X.t"
 |}]
 
 (* Cycle with unequal parameters *)
@@ -86,9 +84,8 @@ Lines 2-6, characters 2-5:
 5 |     type ('a, 'b) t = ('b, 'a) Y.t
 6 |   end
 Error: The definition of "X.t" contains a cycle:
-         "('a, 'b) Y.t" = "('b, 'a) X.t",
-         "('b, 'a) X.t" = "('a, 'b) Y.t",
-         "('a, 'b) Y.t" = "('b, 'a) X.t"
+         "('a, 'b) X.t" = "('c, 'd) Y.t",
+         "('c, 'd) Y.t" = "('d, 'c) X.t"
 |}]
 
 (* Cycle with unequal number of parameters *)
@@ -113,9 +110,8 @@ Lines 2-6, characters 2-5:
 5 |     type ('a, 'b) t = ('b, 'a, bool) Y.t
 6 |   end
 Error: The definition of "X.t" contains a cycle:
-         "('a, 'b, bool) Y.t" = "('b, 'a) X.t",
-         "('b, 'a) X.t" = "('a, 'b, bool) Y.t",
-         "('a, 'b, bool) Y.t" = "('b, 'a) X.t"
+         "('a, 'b) X.t" = "('c, 'd, bool) Y.t",
+         "('c, 'd, bool) Y.t" = "('d, 'c) X.t"
 |}]
 
 (* Cycle is more than just aliasing *)
@@ -127,8 +123,6 @@ Line 1, characters 0-75:
 1 | module rec A : sig type t = B.t -> int end = struct type t = B.t -> int end
     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 Error: The definition of "A.t" contains a cycle:
-         "B.t -> int" contains "B.t",
-         "B.t" = "A.t",
          "A.t" = "B.t -> int",
          "B.t -> int" contains "B.t",
          "B.t" = "A.t"
@@ -159,10 +153,9 @@ Line 9, characters 13-100:
                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 Error: In the signature of this functor application:
        The definition of "Fixed.t" contains a cycle:
-         "Fixed.t" = "Atlas.t -> Fixed.t",
-         "Atlas.t -> Fixed.t" contains "Fixed.t",
          "Fixed.t" = "Fixed.t",
-         "Fixed.t" = "Atlas.t -> Fixed.t"
+         "Fixed.t" = "Atlas.t -> Fixed.t",
+         "Atlas.t -> Fixed.t" contains "Fixed.t"
 |}]
 
 module Constraint(F:sig type 'a t end-> sig type 'a t end) = struct

NickBarnes/oxcaml

testsuite/tests/typing-short-paths/cycles.ml
@@ -31,7 +31,6 @@ Line 2, characters 2-63:
 2 |   module rec X : sig type t = Y.t end = struct type t = Y.t end
       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 Error: The definition of "X.t" contains a cycle:
-         "Y.t" = "X.t",
          "X.t" = "Y.t",
          "Y.t" = "X.t"
 |}]
@@ -59,7 +58,6 @@ Lines 2-6, characters 2-5:
 5 |     type ('a, 'b) t = ('a, 'b) Y.t
 6 |   end
 Error: The definition of "X.t" contains a cycle:
-         "('a, 'b) Y.t" = "('a, 'b) X.t",
          "('a, 'b) X.t" = "('a, 'b) Y.t",
          "('a, 'b) Y.t" = "('a, 'b) X.t"
 |}]
@@ -86,9 +84,8 @@ Lines 2-6, characters 2-5:
 5 |     type ('a, 'b) t = ('b, 'a) Y.t
 6 |   end
 Error: The definition of "X.t" contains a cycle:
-         "('a, 'b) Y.t" = "('b, 'a) X.t",
-         "('b, 'a) X.t" = "('a, 'b) Y.t",
-         "('a, 'b) Y.t" = "('b, 'a) X.t"
+         "('a, 'b) X.t" = "('b, 'a) Y.t",
+         "('b, 'a) Y.t" = "('a, 'b) X.t"
 |}]
 
 (* Cycle with unequal number of parameters *)
@@ -113,9 +110,8 @@ Lines 2-6, characters 2-5:
 5 |     type ('a, 'b) t = ('b, 'a, bool) Y.t
 6 |   end
 Error: The definition of "X.t" contains a cycle:
-         "('a, 'b, bool) Y.t" = "('b, 'a) X.t",
-         "('b, 'a) X.t" = "('a, 'b, bool) Y.t",
-         "('a, 'b, bool) Y.t" = "('b, 'a) X.t"
+         "('a, 'b) X.t" = "('b, 'a, bool) Y.t",
+         "('b, 'a, bool) Y.t" = "('a, 'b) X.t"
 |}]
 
 (* Cycle is more than just aliasing *)
@@ -127,8 +123,6 @@ Line 1, characters 0-75:
 1 | module rec A : sig type t = B.t -> int end = struct type t = B.t -> int end
     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 Error: The definition of "A.t" contains a cycle:
-         "B.t -> int" contains "B.t",
-         "B.t" = "A.t",
          "A.t" = "B.t -> int",
          "B.t -> int" contains "B.t",
          "B.t" = "A.t"
@@ -159,10 +153,9 @@ Line 9, characters 13-100:
                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 Error: In the signature of this functor application:
        The definition of "Fixed.t" contains a cycle:
-         "Fixed.t" = "Atlas.t -> Fixed.t",
-         "Atlas.t -> Fixed.t" contains "Fixed.t",
          "Fixed.t" = "Fixed.t",
-         "Fixed.t" = "Atlas.t -> Fixed.t"
+         "Fixed.t" = "Atlas.t -> Fixed.t",
+         "Atlas.t -> Fixed.t" contains "Fixed.t"
 |}]
 
 module Constraint(F:sig type 'a t end-> sig type 'a t end) = struct